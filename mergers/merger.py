f5abeab28781b8f01199583c704fc8e6ccdfdbd3
Dwayne <93851336+1Iconic1@users.noreply.github.com>
2022-07-21 11:54:42 -0500

    Additional Comments
    Provided additional comments to certain lines of code. Also updated the formatting for final_compiled by fixing the column names to match the STN schema (within stn_merger function).

--- mergers/merger.py
+++ mergers/merger.py
@@ -8,6 +8,8 @@ mpv= pd.read_csv('normal_mpv.csv')
 wapo= pd.read_csv('normal_wapo.csv')
 final_compiled= pd.read_csv('source_merger.csv')
 compiled= pd.DataFrame()
+
+#creates the columns in compiled
 # for col in stn.columns.tolist():
 #     compiled[col]=''
 
@@ -42,6 +44,7 @@ def update_row(match,row):
 rename={"Victim's name":'name'}
 final_compiled=final_compiled.rename(columns=rename)
 issues={}
+#Columns given priority to stn when compared to compiled dataset
 stn_priority=['Biographical Information','Bio URL',
 'Photo URL',
 'A brief description of the circumstances surrounding the death',
@@ -61,74 +64,70 @@ def flag_check(num):
     # 0- Name
     # 1-Age
     # 2- Dates
-    # 3- City
 
     #if key exits in (compiled), find a matching idex in compiled based on date
     if key[0] in final_compiled.name.values:
-        # print(final_compiled.loc[(final_compiled["name"]== key[0]) ,'Age'].values[0] , key[1])
         if str(final_compiled.loc[(final_compiled["name"]== key[0]) ,'Age'].values[0]) == key[1]:
             idx=final_compiled.index[final_compiled["name"]==key[0]]
     
             match= final_compiled.loc[idx]
 
-            # stn row does not com
+            # stn row does not compiled match
             if stn.loc[num].equals(match) != True:
-                for cell in stn.columns.tolist():  
-
+                for cell in stn.columns.tolist():
+                    
+                    #ignoring empty columns and dates(only temporaily)
                     if cell != 'Unnamed: 40' and cell != 'Unnamed: 41' and cell != "Victim's name" and cell != 'Incident Date (MM/DD/YYYY)':
-                        st= stn.loc[num,cell] 
-                        comp= final_compiled.loc[idx.values[0],cell]
 
-
+                        #sets any na values in compiled dataset to the matching value in stn dataset
                         if pd.isna(final_compiled.loc[idx.values[0],cell]) == True:
                             final_compiled.loc[idx.values[0],cell]=stn.loc[num,cell]
 
                 
                         else:
-                            #priority to wapo in threat lvl,
-                            # nan in stn but not compiled
-                            #added information(brief description of circumstances priority over compiled)
-                            # if != or comp exists within st
-                            #'Unarmed/Did Not Have an Actual Weapon'- prioirty to stn
-                            #'Alleged Threat Level (Source: WaPo)' priority to stn
-                            #'Official disposition of death (justified or other)'
-                            #Fleeing(Source:Wapo)
-                            #Criminal Charges
-                            #'Alleged Weapon (Source: WaPo and Review of Cases Not Included in WaPo Database)'
+                            #checks if cell is given priority
                             if cell in stn_priority:
                                 final_compiled.loc[idx.values[0],cell]=stn.loc[num,cell]
+                            # if a cell has an edit distance higher than 5, stn has an existing value in that cell and that existing value does not exist within the the match in compiled
                             else:
                                 if editdistance.eval(str(stn.loc[num,cell]), str(final_compiled.loc[idx.values[0],cell])) > 5 and pd.isna(stn.loc[num,cell]) != True and str(final_compiled.loc[idx.values[0],cell]) not in str(stn.loc[num,cell]) :
                                     issues[num]=(f"Row {num} has a confliction with its match {idx.values[0]}")
                         
     else:
+        #assiging names that do not exist in compiled
         final_compiled.loc[len(compiled.index)]= stn.loc[num]
-
 
 
 
 def source_merger():
     global wapo
     global compiled
+    
     for row in mpv.index:
+        #if a wapo ID exists in both mpv and wapo, find its match and update compiled with this row
         if mpv.loc[row,'WaPo ID (If included in WaPo database)'] != 0:
             match=find_match(row)
+            
+            #if a match exists, update compiled with this row
             if match.empty is not True:
                 final,wapo=update_row(match,row)
                 compiled.loc[len(compiled.index)]= final
         else:
             compiled.loc[len(compiled.index)]= mpv.loc[row]
-    for row in wapo.index: 
+    for row in wapo.index:
+        #appends the remaining wapo rows that do not exist in mpv
         compiled.loc[len(compiled.index)]= wapo.loc[row]
     compiled.to_csv('source_merger.csv')
 
 def stn_merger():
     for row in stn.index:
        flag_check(row)
+    # reformats final_compiled
+    rename={'name':"Victim's name"}
+    final_compiled=final_compiled.rename(columns=rename)
     final_compiled.to_csv('merged.csv')
 
        
-    print(len(issues))
 # source_merger()
 stn_merger()
     
